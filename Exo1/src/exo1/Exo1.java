60258b4477d2f339291b4bd39cec173e6619297c
eloi <eloi@localhost>
2024-09-22 11:24:36 +0200

    Création d'un nouveau projet versionné

--- Exo1/src/exo1/Exo1.java
+++ Exo1/src/exo1/Exo1.java
@@ -1,6 +1,7 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * Artero
+ * TP0 Exo1
+ * Septembre 2024
  */
 package exo1;
 
