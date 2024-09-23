f46fd37df20758054c9039e064578bfa8356f206
eloi <eloi@localhost>
2024-09-23 17:55:19 +0200

    Création d'un nouveau projet versionné

---  TP1_manipNombresInt_Artero/src/tp1_manipnombresint_artero/TP1_manipNombresInt_Artero.java
+++  TP1_manipNombresInt_Artero/src/tp1_manipnombresint_artero/TP1_manipNombresInt_Artero.java
@@ -30,9 +30,9 @@ sous=ope1-ope2;
 int mult;
 mult=ope1*ope2;
 int div;
-div=ope1/ope2;
+div= (ope1*1.0)/(ope2*1.0);
 double reste;
-reste= div -ope1%ope2;
+reste= (float) div -ope1%ope2;
 System.out.println("L'addition: " + add + "\nLa soustraction: " + sous + "\nLa multiplication: " + mult +"\nQuotient entier: "+ div 
 + "\n Reste: " +reste); 
     
