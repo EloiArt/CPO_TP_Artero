6c30c0d6036e2e3aae2b23f121536ea1db401498
eloi <eloi@localhost>
2024-09-23 18:14:17 +0200

    Création d'un nouveau projet versionné

--- TP1_convertisseur _ ARTERO/src/tp1_convertisseur/pkg_/artero/TP1_convertisseur_ARTERO.java
+++ TP1_convertisseur _ ARTERO/src/tp1_convertisseur/pkg_/artero/TP1_convertisseur_ARTERO.java
@@ -4,6 +4,8 @@
  */
 package tp1_convertisseur.pkg_.artero;
 
+import java.util.Scanner;
+
 /**
  *
  * @author eloi
@@ -14,7 +16,12 @@ public class TP1_convertisseur_ARTERO {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-    
+    double val;
+       System.out.println("Veuillez choisir une valeur"); 
+        Scanner sc;
+sc = new Scanner(System.in);  
+val = sc.nextDouble();
+System.out.println("Votre valeur: "+val); 
     }
     
 }
