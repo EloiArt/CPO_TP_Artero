4d51018385e8ba79c7e069a1360a0660d6c7f42f
eloi <eloi@localhost>
2024-09-23 16:38:54 +0200

    Création d'un nouveau projet versionné

--- Calculator/src/calculator/Calculator.java
+++ Calculator/src/calculator/Calculator.java
@@ -14,7 +14,7 @@ public class Calculator {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Please enter the operator");
+        System.out.println("Please enter the operator\n 1) add\n 2) substract\n 3) multiply\n 4) divide\n 5) modulo");
     }
     
 }
